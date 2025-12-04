d3284f6dac4ae767df47e5ebdf16a089fb27bd72
Akhil Shukla <thesquareboy44@gmail.com>
2025-12-04 20:43:27 +0530

    Adding task 1 Resources

---
+++ Task1_VPC/Readme.md
@@ -0,0 +1,10 @@
+VPC CIDR: 10.0.0.0/16 
+Since it is a standard industry practice and provides ~65,000 IPs
+
+for Public Subnet 1: 10.0.1.0/24 (The Availability Zone is eu-north-1a)
+
+for Public Subnet 2: 10.0.2.0/24 (The Availability Zone is eu-north-1b) 
+
+for Private Subnet 1: 10.0.3.0/24 (The Availability Zone is eu-north-1a)
+
+for Private Subnet 2: 10.0.4.0/24 (The Availability Zone is eu-north-1b)
---
+++ Task1_VPC/main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "Akhil_Shukla_VPC"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Akhil_Shukla_IGW"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a" 
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Akhil_Shukla_Public_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24" 
+  availability_zone       = "eu-north-1b" 
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Akhil_Shukla_Public_Subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-north-1a"
+  tags = {
+    Name = "Akhil_Shukla_Private_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24" 
+  availability_zone = "eu-north-1b"
+  tags = {
+    Name = "Akhil_Shukla_Private_Subnet_2"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id 
+  tags = {
+    Name = "Akhil_Shukla_NAT"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Akhil_Shukla_Public_RT"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+  tags = {
+    Name = "Akhil_Shukla_Private_RT"
+  }
+}
+
+resource "aws_route_table_association" "pub_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "pub_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "priv_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "priv_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
